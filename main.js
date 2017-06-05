2d822b932bdccc0f43c49ee7fee58c2616906937
Marta Bermejo <m.berlo8583@gmail.com>
2017-06-05 18:15:16 +0200

    versión sin footer ni mapa

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var toggleMenu = function (event) {
+  var menu = document.getElementById('js-menu-small');
+  if (menu.classList.contains('hide')) {
+    menu.classList.remove('hide');
+  } else {
+    menu.classList.add('hide');
+  }
+}
+document.getElementById('js-button-burguer').addEventListener('click', toggleMenu);
+
+var smallMenuLinkList = document.querySelectorAll('#js-menu-small li a');
+for (var i = 0; i < smallMenuLinkList.length; i++) {
+  smallMenuLinkList[i].addEventListener('click', toggleMenu);
+}
